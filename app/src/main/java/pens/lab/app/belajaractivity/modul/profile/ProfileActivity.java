d685b81767a5969cf78e84c3bf5eca68a5e3fffe
gilangTA <gilangtaufik23@gmail.com>
2020-10-05 10:32:27 +0700

    skeleton

---
+++ app/src/main/java/pens/lab/app/belajaractivity/modul/profile/ProfileActivity.java
@@ -0,0 +1,16 @@
+package pens.lab.app.belajaractivity.modul.profile;
+
+import android.view.View;
+
+import pens.lab.app.belajaractivity.base.BaseFragmentHolderActivity;
+
+
+public class ProfileActivity extends BaseFragmentHolderActivity {
+    ProfileFragment profileFragment;
+
+    @Override
+    protected void initializeFragment() {
+        initializeView();
+
+    }
+}
---
+++ app/src/main/java/pens/lab/app/belajaractivity/modul/profile/ProfileFragment.java
@@ -0,0 +1,40 @@
+package pens.lab.app.belajaractivity.modul.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.Nullable;
+
+import pens.lab.app.belajaractivity.R;
+import pens.lab.app.belajaractivity.base.BaseFragment;
+
+
+/**
+ * Created by fahrul on 13/03/19.
+ */
+
+public class ProfileFragment extends BaseFragment<ProfileActivity, ProfileContract.Presenter> implements ProfileContract.View {
+
+    public ProfileFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        fragmentView = inflater.inflate(R.layout.fragment_profile, container, false);
+        mPresenter = new ProfilePresenter(this);
+        mPresenter.start();
+
+
+        setTitle("Profile");
+        return fragmentView;
+    }
+
+
+    @Override
+    public void setPresenter(ProfileContract.Presenter presenter) {
+        mPresenter = presenter;
+    }
+}
